9773786798263af0fd132aad8559f4a6d2df8ef5
Ian Grant <igrant@nevada.unr.edu>
2018-12-17 17:43:52 -0800

    output folder

---
+++ src/output/printInt.asm
@@ -0,0 +1,13 @@
+#prints a char and then a new line
+printInt:
+
+    #call print-int syscall
+    addi $v0 $zero 1
+    syscall
+
+    #print space 
+    addi $v0 $zero 11
+    addi $a0 $zero 32
+    syscall
+
+    jr $ra
