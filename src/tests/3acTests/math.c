e1996f7375ead270f99bfe51f0e9055fee7a91b9
Ian Grant <igrant@nevada.unr.edu>
2018-12-19 00:42:42 -0800

    forgot dst reg lol, fix some tst cases

--- src/tests/3acTests/math.c
+++ src/tests/3acTests/math.c
@@ -1,18 +1,18 @@
 void printInt(int a);
 int main(){
     int a = 5;
-    printInt(a);
     int h = 4 + a;
-    printInt(h);
     int b = a + 4;
-    printInt(b);
     int c = 5;
-    printInt(c);
     int d = a + 5 - 3;
+    int e = 5 % 10;
+    int f = 2 * a + 3 * b - 5 * c * d / e;
+    printInt(a);
+    printInt(h);
+    printInt(b);
+    printInt(c);
     printInt(d);
-    int e = 10 % 5;
     printInt(e);
-    int f = a + b - c * d / e;
     printInt(f);
     a++;
     a--;
